484021b4a0a36dcfff00987f83b67bc77841dd4d
Pedro Henrique Gonçalves Cruz <cruz.goncalves@aluno.ifsp.edu.br>
2026-09-17 16:16:10 -0300

    Segunda versão

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ const db = require("./db")
 const path = require("path")
 const port = 3000
 
+app.use(express.static('public'));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res) => {
     res.render("index")
+})
+
+app.get("/explorar", (req, res) => {
+    res.render("explorar")
+})
+
+app.get("/postar", (req, res) => {
+    res.render("postar")
 })
 
 app.listen(port, () => {
